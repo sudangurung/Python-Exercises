862f3a204de7f5e1e471bf1b78f76bc5f63a45fa
Sudan Gurung <sudangurung13@gmail.com>
2020-07-09 17:09:56 +0100

    Product Class

--- Question4.py
+++ Question4.py
@@ -1,3 +1,12 @@
+'''
+2.	Write a class called Product. The class should have fields called name, amount, and price, holding the product’s
+name, the number of items of that product in stock, and the regular price of the product. There should be a method
+get_price that receives the number of items to be bought and return a the cost of buying that many items, where the
+regular price is charged for orders of less than 10 items, a 10% discount is applied for orders of between 10 and 99
+items, and a 20% discount is applied for orders of 100 or more items. There should also be a method called make_purchase
+that receives the number of items to be bought and decrease amount by that much.
+'''
+
 
 class Product:
     def __init__(self, name, amount, price):
